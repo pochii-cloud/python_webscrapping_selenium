66280ceea21faa2daa673e5391771f77b68658aa
pochii <johnmwanikig2000@gmail.com>
2021-11-07 13:34:31 +0300

    commit two

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
 
+driver.get("https://techwithtim.net/")
+link = driver.find_element_by_link_text("Python Programming")
+link.click()
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
+    )
+    element.click()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+    )
+    element.click()
 
+    driver.back()
+    driver.back()
+    driver.back()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+except:
+    driver.quit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print(driver.title)
+# search = driver.find_element_by_name("s")
+# search.send_keys("test")
+# search.send_keys(Keys.RETURN)
+#
+# try:
+#     main = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.ID, "main"))
+#     )
+#     articles = main.find_elements_by_tag_name("article")
+#     for article in articles:
+#         header = article.find_element_by_class_name("entry-summary")
+#         print(header.text)
+# finally:
+#     driver.quit()
